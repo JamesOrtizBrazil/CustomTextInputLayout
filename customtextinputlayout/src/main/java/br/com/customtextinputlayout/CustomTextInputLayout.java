58d4ae95a7a5c915d6f07f51fed2c7444847fb4d
JamesOrtiz <jamesortiz@gmail.com>
2019-11-14 15:17:02 -0300

    disable paste on spinner

--- customtextinputlayout/src/main/java/br/com/customtextinputlayout/CustomTextInputLayout.java
+++ customtextinputlayout/src/main/java/br/com/customtextinputlayout/CustomTextInputLayout.java
@@ -203,7 +203,26 @@ public class CustomTextInputLayout
                 }
             });
 
+            //disable paste
+            customSpinner.setCustomSelectionActionModeCallback(new ActionMode.Callback() {
+                public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                public void onDestroyActionMode(ActionMode mode) {
+                }
+
+                public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                    return false;
+                }
+
+                public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                    return false;
+                }
+            });
+
             customSpinner.setLongClickable(false);
+            customSpinner.setTextIsSelectable(false);
         } else {
             editText = new TextInputEditText(getContext());
             editText.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
